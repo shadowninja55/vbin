dbcc769eb9fe65d6d16e856051c5d9243435b586
shadowninja55 <shadowninja1050@gmail.com>
2021-02-10 11:03:42 -0500

    integrated frontend + backend for upload fetch

--- backend.v
+++ backend.v
@@ -79,25 +79,42 @@ fn (mut app App) index() vweb.Result {
 	return $vweb.html()
 }
 
-[get]
-["/api/fetch"] 
-fn (mut app App) fetch() vweb.Result {
-	code := app.query["code"]
-
-	if _unlikely_(code == "") {
-		return app.text("no code was supplied")
+pub fn lookup(db sqlite.DB, code string) ?string {
+	if code == "" {
+		return error("no code was supplied")
 	} else {
-		entries := sql app.db {
+		entries := sql db {
 			select from Upload where code == code
 		}
 
 		if entries.len == 0 {
-			return app.text("an invalid code was supplied")
+			return error("an invalid code was supplied")
 		}
 	}
 
-	content := os.read_file("uploads/${code}.txt") or {
-		return app.server_error(500)
+	content := os.read_file("uploads/${code}.txt") or { 
+		return error("os error") 
+	}
+
+	return content
+}
+
+[get]
+["/uploads/:code"]
+fn (mut app App) upload(code string) vweb.Result {
+	res := lookup(app.db, code) or {
+		return app.text(err)
+	}
+
+	content := res
+	return $vweb.html()
+}
+
+[get]
+["/api/fetch"] 
+fn (mut app App) api_fetch() vweb.Result {
+	content := lookup(app.db, app.query["code"]) or {
+		return app.text(err)
 	}
 
 	return app.text(content)
@@ -105,7 +122,7 @@ fn (mut app App) fetch() vweb.Result {
 
 [post]
 ["/api/upload"]
-fn (mut app App) upload() vweb.Result {
+fn (mut app App) api_upload() vweb.Result {
 	content := app.form["content"]
 
 	if _unlikely_(content == "") {
@@ -132,6 +149,9 @@ fn (mut app App) upload() vweb.Result {
 
 pub fn (mut app App) init_once() {
 	app.handle_static("templates")
+	app.serve_static("/static/prism.js", "static/js/prism.js", "text/javascript")
+	app.serve_static("/static/prism.css", "static/css/prism.css", "text/css")
+	app.serve_static("/static/bulma.min.css", "static/css/bulma.min.css", "text/css")
 
 	app.db = sqlite.connect("db/vbin.sqlite") or {
 		panic("vbin.sqlite not found!") 
